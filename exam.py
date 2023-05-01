8a44ccf94e47929804073891f77632e19d1c781c
2boss11 <tunbosunbanjo@hotmail.com>
2023-05-01 15:54:19 +0100

    first commit

---
+++ exam.py
@@ -0,0 +1,54 @@
+/ PYTHON
+/*
+2.
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+area = length * width
+perimeter = 2 * (length + width)
+
+    print("Area of the rectangle:", area)
+    print("Perimeter of the rectangle:", perimeter)*/
+
+
+3
+radius = float(input("Enter the radius of the circle: "))
+
+pi = 3.14
+area = pi * radius * radius
+circumference = 2 * pi * radius
+
+    print("Area of the circle:", area)
+    print("Circumference of the circle:", circumference)
+
+*/
+
+/*
+7
+
+ages = [22, 19, 24, 25, 26, 24, 25, 24]
+
+ages_set = set(ages)
+
+if len(ages) > len(ages_set):
+    print("The list is bigger than the set.")
+elif len(ages) < len(ages_set):
+    print("The set is bigger than the list.")
+else:
+    print("The list and the set have the same length.")*/
+
+
+// 8
+// Strings are characters enclosed in quotes
+// A list is a sequence of values enclosed in a square bracket. it is mutable.
+// Tuple is a sequence of values enclosed in a square bracket. it is immutable.
+// Set is a collection of unique values, enclosed in curly braces. it can be mutable but not duplicated
+
+
+// 12
+/*
+numbers = [-4, -3, -2, -1, 0, 2, 4, 6]
+
+filteredNumbers = [num for num in numbers if num <= 0]
+
+print(filteredNumbers)*/
